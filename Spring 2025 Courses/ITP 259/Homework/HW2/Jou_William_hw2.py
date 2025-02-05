5e2260d21e397f761bb3bf4fdfe68c3ffa82e6af
William Jou <wcjou@usc.edu>
2025-02-04 22:12:12 -0800

    .

--- Spring 2025 Courses/ITP 259/Homework/HW2/Jou_William_hw2.py
+++ Spring 2025 Courses/ITP 259/Homework/HW2/Jou_William_hw2.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import Normalizer
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -20,3 +21,34 @@ print(winedf)
 print(wine_quality)
 
 norm = Normalizer()
+
+winedf_norm = pd.DataFrame(norm.transform(winedf), columns=winedf.columns)
+
+print(winedf_norm)
+
+
+ks = range(1, 11)
+inertias = []
+for k in ks:
+    model = KMeans(n_clusters=k)
+    model.fit(winedf_norm)
+    inertias.append(model.inertia_)
+
+plt.plot(ks, inertias, "-o")
+plt.xlabel("Number of Clusters, k")
+plt.ylabel("Inertia")
+plt.xticks(ks)
+plt.show()
+
+# You would pick 5 clusters 20%/elbow rule
+
+model = KMeans(n_clusters=5, random_state=2023)
+model.fit(winedf_norm)
+labels = model.predict(winedf_norm)
+winedf_norm['cluster'] = pd.Series(labels)
+
+print(winedf_norm)
+
+winedf_norm['quality'] = wine_quality
+
+print(pd.crosstab(winedf_norm['quality'], winedf_norm['cluster']))
