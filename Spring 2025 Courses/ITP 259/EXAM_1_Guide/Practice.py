4efe31ee57f19ad57e2c1733dc721f10f4ea1d17
William Jou <williamthejou@gmail.com>
2025-03-04 15:37:58 -0800

    .

--- Spring 2025 Courses/ITP 259/EXAM_1_Guide/Practice.py
+++ Spring 2025 Courses/ITP 259/EXAM_1_Guide/Practice.py
@@ -1,4 +1,4 @@
-import pandas as pd
+# import pandas as pd
 
 #HW 1
 
@@ -29,22 +29,22 @@ import pandas as pd
 
 #HW 2
 
-import pandas as pd
-from sklearn.preprocessing import Normalizer
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
+# import pandas as pd
+# from sklearn.preprocessing import Normalizer
+# from sklearn.cluster import KMeans
+# import matplotlib.pyplot as plt
 
-df = pd.read_csv('Spring 2025 Courses/ITP 259/EXAM_1_Guide/wineQualityReds.csv')
+# df = pd.read_csv('Spring 2025 Courses/ITP 259/EXAM_1_Guide/wineQualityReds.csv')
 
-df.drop('Wine', axis=1, inplace=True)
+# df.drop('Wine', axis=1, inplace=True)
 
-quality = df['quality']
+# quality = df['quality']
 
-df.drop('quality', axis=1, inplace=True)
+# df.drop('quality', axis=1, inplace=True)
 
-norm = Normalizer()
+# norm = Normalizer()
 
-df_norm = pd.DataFrame(norm.transform(df), columns = df.columns)
+# df_norm = pd.DataFrame(norm.transform(df), columns = df.columns)
 
 # print(df_norm)
 
@@ -73,4 +73,98 @@ df_norm = pd.DataFrame(norm.transform(df), columns = df.columns)
 # print(pd.crosstab(df_norm['quality'], df_norm['cluster']))
 
 #HW 3
+
+import pandas as pd
+import numpy as np
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+
+# df = pd.read_csv('Spring 2025 Courses/ITP 259/EXAM_1_Guide/Titanic.csv')
+
+# # print(df.head())
+# survived = df['Survived']
+
+# df_new = df.drop(['Passenger', 'Survived'], axis=1)
+# df_dummy = pd.get_dummies(df_new, prefix=['Class','Sex', 'Age'])
+# # print(df_dummy)
+
+# X = df_dummy 
+# y = survived
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2023)
+
+# log_reg = LogisticRegression()
+# log_reg.fit(X_train, y_train)
+
+# y_pred = log_reg.predict(X_test)
+# # print(metrics.accuracy_score(y_test, y_pred))
+
+# cm = confusion_matrix(y_test, y_pred)
+
+# ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=log_reg.classes_).plot()
+
+# # plt.show()
+
+# # print(df_dummy)
+# print(log_reg.predict([[0, 0, 1, 0, 0, 1, 1, 0]]))
+
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+
+# diabetes_knn = pd.read_csv('Spring 2025 Courses/ITP 259/EXAM_1_Guide/diabetes.csv')
+
+# print(diabetes_knn.head())
+# outcome = diabetes_knn['Outcome']
+
+# X = diabetes_knn.drop('Outcome', axis=1)
+# y = outcome
+
+# scaler = StandardScaler()
+
+# scaler.fit(X)
+# X_scaled = scaler.transform(X)
+
+# X_train_A, X_temp, y_train_A, y_temp = train_test_split(X_scaled, y, test_size=0.4, random_state=2023, stratify=y)
+# X_train_B, X_test, y_train_B, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=2023, stratify=y_temp)
+
+# ks = range(1, 31)
+# train_A_scores = []
+# train_B_scores = []
+
+# for k in ks:
+#     knn = KNeighborsClassifier(n_neighbors=k)
+#     knn.fit(X_train_A, y_train_A)
+#     y_pred_A = knn.predict(X_train_A)
+#     y_pred_B = knn.predict(X_train_B)
+#     y_pred = knn.predict(X_test)
+#     train_A_scores.append(metrics.accuracy_score(y_train_A, y_pred_A))
+#     train_B_scores.append(metrics.accuracy_score(y_train_B, y_pred_B))
+
+# plt.title("KNN: Varying number of neighbors")
+# plt.plot(ks, train_A_scores, label="Training A Accuracy")
+# plt.plot(ks, train_B_scores, label = "Training B Accuracy")
+# plt.legend()
+# plt.xlabel("Number of Neighbors")
+# plt.ylabel("Accuracy")
+# plt.show()
+
+# knn = KNeighborsClassifier(n_neighbors=10)
+# knn.fit(X_train_A, y_train_A)
+# y_pred = knn.predict(X_test)
+
+# # print(metrics.accuracy_score(y_test, y_pred))
+
+# # cm = confusion_matrix(y_test, y_pred)
+# # ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=knn.classes_).plot()
+
+# # plt.show()
+
+
+# print(knn.predict([[2, 150, 85, 22, 200, 30, 0.3, 55]]))
+
 
