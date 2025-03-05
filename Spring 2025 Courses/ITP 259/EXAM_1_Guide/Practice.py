3807917a57a71f832f31a143a4d07374226e29da
William Jou <wcjou@usc.edu>
2025-03-05 01:05:54 -0800

    .

--- Spring 2025 Courses/ITP 259/EXAM_1_Guide/Practice.py
+++ Spring 2025 Courses/ITP 259/EXAM_1_Guide/Practice.py
@@ -18,4 +18,17 @@ df = pd.read_csv('USC-Spring-2025-\Spring 2025 Courses\ITP 259\EXAM_1_Guide\wine
 # print(df[df['alcohol'] > 10].shape[0])
 
 #7
-print(df.loc[(df['alcohol'].idxmax())])
+# print(df.loc[(df['alcohol'].idxmax())])
+
+#8
+# random_wine = df.sample(1)
+# print(random_wine['residual.sugar'].values[0])
+
+#9
+# qual_4 = df[df['quality'] == 4]
+# print(qual_4.sample(1))
+
+#10
+# print(df.shape[0])
+# df = df[df['quality'] != 4]
+# print(df.shape[0])
